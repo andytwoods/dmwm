fab47acdcbb732059783d7f5e90eb8ee84e08d65
ugwf004 <This4Spore>
2019-08-27 11:27:21 +0100

    Added new fields, changed the layout

--- crunch/models.py
+++ crunch/models.py
@@ -29,6 +29,14 @@ Company_Focus_Area_Choice_dict = {x: y for x, y in Company_Focus_Area_Choice}
 
 Within_Gateway_cluster_Choice = (('YE', "Yes"), ('NO', "No"), ('MA', "Marginal"))
 
+Unqualified_type_Choice = (('ST', "Sole Trader/Individual"),
+                           ('HE', "HEI"),
+                           ('SM', "SME"),
+                           ('IP', "Industry Partner"),
+                           ('SO', "Support Org"),
+                           )
+
+Experience_in_immersive = (('NO', "Novice"), ('IN', "Intermediate"), ('EX', "Expert"))
 
 Within_Gateway_cluster_Choice_dict = {x: y for x, y in Within_Gateway_cluster_Choice}
 
@@ -41,30 +49,36 @@ class ZohoInfo(models.Model):
     Registered_Address = models.CharField(blank=True, max_length=255, null=True)
     Registered_Town_City = models.CharField(blank=True, max_length=255, null=True)
     Registered_Post_Code = models.CharField(blank=True, max_length=255, null=True)
-    Email = models.EmailField(blank=True, null=True)
-    Website = models.URLField(blank=True, null=True)
-    Phone = models.IntegerField(blank=True, null=True)
-    Twitter = models.CharField(blank=True, max_length=255, null=True)
-    Company_Reg_Number_or_UTR = models.CharField(blank=True, max_length=255, null=True)
-    Your_Company_SIC_Code = models.CharField(blank=True, max_length=255, null=True)
-
-    Clusters_TTWA = models.CharField(max_length=5, choices=Clusters_TTWA_Choice, null=True, blank=True)
-    Company_Focus_Area = models.CharField(max_length=5, choices=Company_Focus_Area_Choice, null=True, blank=True)
-    Within_Gateway_cluster = models.CharField(max_length=5, choices=Within_Gateway_cluster_Choice, null=True, blank=True)
-
-    Unqualified_Status = models.CharField(blank=True, max_length=255, null=True)
-    Unqualified_Source = models.CharField(blank=True, max_length=255, null=True)
     Street = models.CharField(blank=True, max_length=255, null=True)
     City = models.CharField(blank=True, max_length=255, null=True)
     Province = models.CharField(blank=True, max_length=255, null=True)
     Postal_Code = models.CharField(blank=True, max_length=255, null=True)
     Country = models.CharField(blank=True, max_length=255, null=True, default='UK')
+    Clusters_TTWA = models.CharField(max_length=5, choices=Clusters_TTWA_Choice, null=True, blank=True)
+    Within_Gateway_cluster = models.CharField(max_length=5, choices=Within_Gateway_cluster_Choice, null=True,
+                                              blank=True)
+
     Last_Name = models.CharField(blank=True, max_length=255, null=True, default='n/a')
     First_Name = models.CharField(blank=True, max_length=255, null=True, )
     Job_Title = models.CharField(blank=True, max_length=255, null=True, )
+    Email = models.EmailField(blank=True, null=True)
+    Company_Generic_Email = models.EmailField(blank=True, null=True)
+    Website = models.URLField(blank=True, null=True)
+    Phone = models.IntegerField(blank=True, null=True)
+    Twitter = models.CharField(blank=True, max_length=255, null=True)
 
-    No_of_Employees = models.SmallIntegerField(blank=True, null=True)
+    Company_Focus_Area = models.CharField(max_length=5, choices=Company_Focus_Area_Choice, null=True, blank=True)
+    Company_Focus_Area_Detail = models.CharField(blank=True, max_length=255, null=True)
+    Company_Reg_Number_or_UTR = models.CharField(blank=True, max_length=255, null=True)
+    Your_Company_SIC_Code = models.CharField(blank=True, max_length=255, null=True)
+    About_the_Company = models.CharField(blank=True, max_length=455, null=True)
+    Experience_in_immersive = models.Charfield(ax_length=5, choices=Experience_in_immersive, null=True, blank=True)
     Year_Company_Was_Founded = models.IntegerField(blank=True, null=True)
 
+    Unqualified_Status = models.CharField(blank=True, max_length=255, null=True)
+    Unqualified_Source = models.CharField(blank=True, max_length=255, null=True)
+    No_of_Employees = models.SmallIntegerField(blank=True, null=True)
+    Cluster = models.BooleanField(default=True)
+    Unqualified_type_Choice = models.CharField(max_length=5, choices=Unqualified_type_Choice, null=True, blank=True)
     Unqualified_Owner = models.CharField(blank=True, max_length=255, null=True, default='Marianna Rolbina')
     Unqualified_Owner_Id = models.IntegerField(blank=True, null=True, default='167499000000416001')
